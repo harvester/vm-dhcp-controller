b22d57cf526b4793e7bda6b4073de41e2762c226
Zespre Chang <zespre.chang@suse.com>
2023-12-23 15:54:45 +0800

    fix(crd): add custom printed columns
    Signed-off-by: Zespre Chang <zespre.chang@suse.com>

--- pkg/crd/crd.go
+++ pkg/crd/crd.go
@@ -4,20 +4,38 @@ import (
 	"context"
 
 	"github.com/rancher/wrangler/pkg/crd"
+	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/rest"
 
 	network "github.com/starbops/vm-dhcp-controller/pkg/apis/network.harvesterhci.io/v1alpha1"
 )
 
+var ageColumn = apiextv1.CustomResourceColumnDefinition{
+	Name:     "AGE",
+	Type:     "date",
+	Priority: 10,
+	JSONPath: ".metadata.creationTimestamp",
+}
+
 func List() []crd.CRD {
 	return []crd.CRD{
 		newCRD("network.harvesterhci.io", &network.IPPool{}, func(c crd.CRD) crd.CRD {
+			c.NonNamespace = true
 			return c.
-				WithColumn("Available", ".status.ipv4.available").
-				WithColumn("Used", ".status.ipv4.used")
+				WithShortNames("ipl", "ipls").
+				WithColumn("NETWORK", ".spec.networkName").
+				WithColumn("AVAILABLE", ".status.ipv4.available").
+				WithColumn("USED", ".status.ipv4.used").
+				WithCustomColumn(ageColumn)
 		}),
-		newCRD("network.harvesterhci.io", &network.VirtualMachineNetworkConfig{}, nil),
+		newCRD("network.harvesterhci.io", &network.VirtualMachineNetworkConfig{}, func(c crd.CRD) crd.CRD {
+			return c.
+				WithShortNames("vmnetcfg", "vmnetcfgs").
+				WithColumn("VMNAME", ".spec.vmName").
+				WithColumn("NETWORK", ".spec.networkConfig.networkName").
+				WithCustomColumn(ageColumn)
+		}),
 	}
 }
 
