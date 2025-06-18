26219363bb2349a5c1743b7cbd6b9e667bc9bb46
davidepasquero <57875308+davidepasquero@users.noreply.github.com>
2025-06-18 18:06:44 +0200

    cleanup: remove stuck pod finalizers
    Signed-off-by: Pasquero Davide 2204 <davide.pasquero@csi.it>

--- pkg/util/pod.go
+++ pkg/util/pod.go
@@ -8,6 +8,7 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 )
 
@@ -44,6 +45,14 @@ func CleanupTerminatingPods(ctx context.Context, client kubernetes.Interface, na
 				}
 
 				logrus.Infof("(CleanupTerminatingPods) force deleting stuck pod %s/%s", pod.Namespace, pod.Name)
+
+				if len(pod.Finalizers) > 0 {
+					patch := []byte(`{"metadata":{"finalizers":null}}`)
+					if _, err := client.CoreV1().Pods(pod.Namespace).Patch(ctx, pod.Name, types.MergePatchType, patch, metav1.PatchOptions{}); err != nil && !apierrors.IsNotFound(err) {
+						logrus.Errorf("(CleanupTerminatingPods) patch pod %s/%s error: %v", pod.Namespace, pod.Name, err)
+					}
+				}
+
 				grace := int64(0)
 				if err := client.CoreV1().Pods(pod.Namespace).Delete(ctx, pod.Name, metav1.DeleteOptions{GracePeriodSeconds: &grace}); err != nil && !apierrors.IsNotFound(err) {
 					logrus.Errorf("(CleanupTerminatingPods) delete pod %s/%s error: %v", pod.Namespace, pod.Name, err)
