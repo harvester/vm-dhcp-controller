a60f7a764d2d8a95aa3ea1898a9070aa3683b3d1
Zespre Chang <zespre.chang@suse.com>
2026-07-05 15:52:02 +0800

    feat(webhook): validate desired static IPs on vmnetcfg
    Reject desired IPs outside the pool, reserved or excluded addresses, duplicates, and addresses allocated to another MAC.
    
    Validate updates only when desired IP entries are new or changed so unchanged and removed configs do not require NAD/IPPool lookups.
    
    Signed-off-by: Zespre Chang <zespre.chang@suse.com>

--- pkg/webhook/vmnetcfg/validator_test.go
+++ pkg/webhook/vmnetcfg/validator_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/harvester/webhook/pkg/server/admission"
 	cniv1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
 	"github.com/stretchr/testify/assert"
+	admissionregv1 "k8s.io/api/admissionregistration/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 
 	networkv1 "github.com/harvester/vm-dhcp-controller/pkg/apis/network.harvesterhci.io/v1alpha1"
@@ -24,6 +25,15 @@ const (
 	testNADNamespace      = "default"
 	testNADName           = "test-net"
 	testNetworkName       = testNADNamespace + "/" + testNADName
+	testCIDR              = "192.168.0.0/24"
+	testStartIP           = "192.168.0.100"
+	testEndIP             = "192.168.0.200"
+	testIPAddress1        = "192.168.0.111"
+	testIPAddress2        = "192.168.0.112"
+	testOutsideCIDRIP     = "192.168.1.111"
+	testOutsideRangeIP    = "192.168.0.50"
+	testMACAddress1       = "11:22:33:44:55:66"
+	testMACAddress2       = "22:33:44:55:66:77"
 )
 
 func newTestVirtualMachineNetworkConfigBuilder() *vmnetcfg.VmNetCfgBuilder {
@@ -134,6 +144,129 @@ func TestValidator_Create(t *testing.T) {
 				shouldErr: true,
 			},
 		},
+		{
+			name: "desired static ip inside pool is allowed",
+			given: input{
+				vmNetCfg: newTestVirtualMachineNetworkConfigBuilder().
+					WithNetworkConfig(testIPAddress1, testMACAddress1, testNetworkName).Build(),
+				ipPool: newTestIPPoolBuilder().
+					NetworkName(testNetworkName).
+					CIDR(testCIDR).
+					PoolRange(testStartIP, testEndIP).Build(),
+				nad: newTestNetworkAttachmentDefinitionBuilder().
+					Label(util.IPPoolNamespaceLabelKey, testIPPoolNamespace).
+					Label(util.IPPoolNameLabelKey, testIPPoolName).Build(),
+			},
+			expected: output{
+				shouldErr: false,
+			},
+		},
+		{
+			name: "desired static ip outside cidr is denied",
+			given: input{
+				vmNetCfg: newTestVirtualMachineNetworkConfigBuilder().
+					WithNetworkConfig(testOutsideCIDRIP, testMACAddress1, testNetworkName).Build(),
+				ipPool: newTestIPPoolBuilder().
+					NetworkName(testNetworkName).
+					CIDR(testCIDR).
+					PoolRange(testStartIP, testEndIP).Build(),
+				nad: newTestNetworkAttachmentDefinitionBuilder().
+					Label(util.IPPoolNamespaceLabelKey, testIPPoolNamespace).
+					Label(util.IPPoolNameLabelKey, testIPPoolName).Build(),
+			},
+			expected: output{
+				shouldErr: true,
+			},
+		},
+		{
+			name: "desired static ip outside pool range is denied",
+			given: input{
+				vmNetCfg: newTestVirtualMachineNetworkConfigBuilder().
+					WithNetworkConfig(testOutsideRangeIP, testMACAddress1, testNetworkName).Build(),
+				ipPool: newTestIPPoolBuilder().
+					NetworkName(testNetworkName).
+					CIDR(testCIDR).
+					PoolRange(testStartIP, testEndIP).Build(),
+				nad: newTestNetworkAttachmentDefinitionBuilder().
+					Label(util.IPPoolNamespaceLabelKey, testIPPoolNamespace).
+					Label(util.IPPoolNameLabelKey, testIPPoolName).Build(),
+			},
+			expected: output{
+				shouldErr: true,
+			},
+		},
+		{
+			name: "desired static ip marked excluded is denied",
+			given: input{
+				vmNetCfg: newTestVirtualMachineNetworkConfigBuilder().
+					WithNetworkConfig(testIPAddress1, testMACAddress1, testNetworkName).Build(),
+				ipPool: newTestIPPoolBuilder().
+					NetworkName(testNetworkName).
+					CIDR(testCIDR).
+					PoolRange(testStartIP, testEndIP).
+					Allocated(testIPAddress1, util.ExcludedMark).Build(),
+				nad: newTestNetworkAttachmentDefinitionBuilder().
+					Label(util.IPPoolNamespaceLabelKey, testIPPoolNamespace).
+					Label(util.IPPoolNameLabelKey, testIPPoolName).Build(),
+			},
+			expected: output{
+				shouldErr: true,
+			},
+		},
+		{
+			name: "desired static ip marked reserved is denied",
+			given: input{
+				vmNetCfg: newTestVirtualMachineNetworkConfigBuilder().
+					WithNetworkConfig(testIPAddress1, testMACAddress1, testNetworkName).Build(),
+				ipPool: newTestIPPoolBuilder().
+					NetworkName(testNetworkName).
+					CIDR(testCIDR).
+					PoolRange(testStartIP, testEndIP).
+					Allocated(testIPAddress1, util.ReservedMark).Build(),
+				nad: newTestNetworkAttachmentDefinitionBuilder().
+					Label(util.IPPoolNamespaceLabelKey, testIPPoolNamespace).
+					Label(util.IPPoolNameLabelKey, testIPPoolName).Build(),
+			},
+			expected: output{
+				shouldErr: true,
+			},
+		},
+		{
+			name: "desired static ip allocated to a different mac is denied",
+			given: input{
+				vmNetCfg: newTestVirtualMachineNetworkConfigBuilder().
+					WithNetworkConfig(testIPAddress1, testMACAddress1, testNetworkName).Build(),
+				ipPool: newTestIPPoolBuilder().
+					NetworkName(testNetworkName).
+					CIDR(testCIDR).
+					PoolRange(testStartIP, testEndIP).
+					Allocated(testIPAddress1, testMACAddress2).Build(),
+				nad: newTestNetworkAttachmentDefinitionBuilder().
+					Label(util.IPPoolNamespaceLabelKey, testIPPoolNamespace).
+					Label(util.IPPoolNameLabelKey, testIPPoolName).Build(),
+			},
+			expected: output{
+				shouldErr: true,
+			},
+		},
+		{
+			name: "duplicate desired static ip on same network is denied",
+			given: input{
+				vmNetCfg: newTestVirtualMachineNetworkConfigBuilder().
+					WithNetworkConfig(testIPAddress1, testMACAddress1, testNetworkName).
+					WithNetworkConfig(testIPAddress1, testMACAddress2, testNetworkName).Build(),
+				ipPool: newTestIPPoolBuilder().
+					NetworkName(testNetworkName).
+					CIDR(testCIDR).
+					PoolRange(testStartIP, testEndIP).Build(),
+				nad: newTestNetworkAttachmentDefinitionBuilder().
+					Label(util.IPPoolNamespaceLabelKey, testIPPoolNamespace).
+					Label(util.IPPoolNameLabelKey, testIPPoolName).Build(),
+			},
+			expected: output{
+				shouldErr: true,
+			},
+		},
 	}
 
 	nadGVR := schema.GroupVersionResource{
@@ -160,4 +293,155 @@ func TestValidator_Create(t *testing.T) {
 		err := validator.Create(&admission.Request{}, tc.given.vmNetCfg)
 		assert.Equal(t, tc.expected.shouldErr, err != nil, tc.name)
 	}
+}
+
+func TestValidator_Update(t *testing.T) {
+	type input struct {
+		oldVmNetCfg *networkv1.VirtualMachineNetworkConfig
+		vmNetCfg    *networkv1.VirtualMachineNetworkConfig
+		ipPool      *networkv1.IPPool
+		nad         *cniv1.NetworkAttachmentDefinition
+	}
+
+	testCases := []struct {
+		name     string
+		given    input
+		expected bool
+	}{
+		{
+			name: "desired static ip inside pool is allowed",
+			given: input{
+				oldVmNetCfg: newTestVirtualMachineNetworkConfigBuilder().
+					WithNetworkConfig("", testMACAddress1, testNetworkName).Build(),
+				vmNetCfg: newTestVirtualMachineNetworkConfigBuilder().
+					WithNetworkConfig(testIPAddress1, testMACAddress1, testNetworkName).Build(),
+				ipPool: newTestIPPoolBuilder().
+					NetworkName(testNetworkName).
+					CIDR(testCIDR).
+					PoolRange(testStartIP, testEndIP).Build(),
+				nad: newTestNetworkAttachmentDefinitionBuilder().
+					Label(util.IPPoolNamespaceLabelKey, testIPPoolNamespace).
+					Label(util.IPPoolNameLabelKey, testIPPoolName).Build(),
+			},
+			expected: false,
+		},
+		{
+			name: "desired static ip outside pool range is denied",
+			given: input{
+				oldVmNetCfg: newTestVirtualMachineNetworkConfigBuilder().
+					WithNetworkConfig("", testMACAddress1, testNetworkName).Build(),
+				vmNetCfg: newTestVirtualMachineNetworkConfigBuilder().
+					WithNetworkConfig(testOutsideRangeIP, testMACAddress1, testNetworkName).Build(),
+				ipPool: newTestIPPoolBuilder().
+					NetworkName(testNetworkName).
+					CIDR(testCIDR).
+					PoolRange(testStartIP, testEndIP).Build(),
+				nad: newTestNetworkAttachmentDefinitionBuilder().
+					Label(util.IPPoolNamespaceLabelKey, testIPPoolNamespace).
+					Label(util.IPPoolNameLabelKey, testIPPoolName).Build(),
+			},
+			expected: true,
+		},
+		{
+			name: "unchanged desired static ip does not require nad or ippool lookup",
+			given: input{
+				oldVmNetCfg: newTestVirtualMachineNetworkConfigBuilder().
+					WithNetworkConfig(testIPAddress1, testMACAddress1, testNetworkName).Build(),
+				vmNetCfg: newTestVirtualMachineNetworkConfigBuilder().
+					WithNetworkConfig(testIPAddress1, testMACAddress1, testNetworkName).Build(),
+			},
+			expected: false,
+		},
+		{
+			name: "removed network config does not require nad or ippool lookup",
+			given: input{
+				oldVmNetCfg: newTestVirtualMachineNetworkConfigBuilder().
+					WithNetworkConfig(testIPAddress1, testMACAddress1, testNetworkName).Build(),
+				vmNetCfg: newTestVirtualMachineNetworkConfigBuilder().Build(),
+			},
+			expected: false,
+		},
+		{
+			name: "removed desired static ip does not require nad or ippool lookup",
+			given: input{
+				oldVmNetCfg: newTestVirtualMachineNetworkConfigBuilder().
+					WithNetworkConfig(testIPAddress1, testMACAddress1, testNetworkName).Build(),
+				vmNetCfg: newTestVirtualMachineNetworkConfigBuilder().
+					WithNetworkConfig("", testMACAddress1, testNetworkName).Build(),
+			},
+			expected: false,
+		},
+		{
+			name: "changed desired static ip requires nad and ippool lookup",
+			given: input{
+				oldVmNetCfg: newTestVirtualMachineNetworkConfigBuilder().
+					WithNetworkConfig(testIPAddress1, testMACAddress1, testNetworkName).Build(),
+				vmNetCfg: newTestVirtualMachineNetworkConfigBuilder().
+					WithNetworkConfig(testIPAddress2, testMACAddress1, testNetworkName).Build(),
+			},
+			expected: true,
+		},
+		{
+			name: "new dynamic network config does not require nad or ippool lookup",
+			given: input{
+				oldVmNetCfg: newTestVirtualMachineNetworkConfigBuilder().Build(),
+				vmNetCfg: newTestVirtualMachineNetworkConfigBuilder().
+					WithNetworkConfig("", testMACAddress1, testNetworkName).Build(),
+			},
+			expected: false,
+		},
+		{
+			name: "changed desired static ip duplicated with unchanged config is denied",
+			given: input{
+				oldVmNetCfg: newTestVirtualMachineNetworkConfigBuilder().
+					WithNetworkConfig("", testMACAddress1, testNetworkName).
+					WithNetworkConfig(testIPAddress2, testMACAddress2, testNetworkName).Build(),
+				vmNetCfg: newTestVirtualMachineNetworkConfigBuilder().
+					WithNetworkConfig(testIPAddress2, testMACAddress1, testNetworkName).
+					WithNetworkConfig(testIPAddress2, testMACAddress2, testNetworkName).Build(),
+				ipPool: newTestIPPoolBuilder().
+					NetworkName(testNetworkName).
+					CIDR(testCIDR).
+					PoolRange(testStartIP, testEndIP).Build(),
+				nad: newTestNetworkAttachmentDefinitionBuilder().
+					Label(util.IPPoolNamespaceLabelKey, testIPPoolNamespace).
+					Label(util.IPPoolNameLabelKey, testIPPoolName).Build(),
+			},
+			expected: true,
+		},
+	}
+
+	nadGVR := schema.GroupVersionResource{
+		Group:    "k8s.cni.cncf.io",
+		Version:  "v1",
+		Resource: "network-attachment-definitions",
+	}
+
+	for _, tc := range testCases {
+		clientset := fake.NewSimpleClientset()
+		if tc.given.ipPool != nil {
+			err := clientset.Tracker().Add(tc.given.ipPool)
+			assert.NoError(t, err, "mock resource should add into fake controller tracker")
+		}
+		if tc.given.nad != nil {
+			err := clientset.Tracker().Create(nadGVR, tc.given.nad, tc.given.nad.Namespace)
+			assert.NoError(t, err, "mock resource should add into fake controller tracker")
+		}
+
+		ipPoolCache := fakeclient.IPPoolCache(clientset.NetworkV1alpha1().IPPools)
+		nadCache := fakeclient.NetworkAttachmentDefinitionCache(clientset.K8sCniCncfIoV1().NetworkAttachmentDefinitions)
+		validator := NewValidator(ipPoolCache, nadCache)
+
+		err := validator.Update(&admission.Request{}, tc.given.oldVmNetCfg, tc.given.vmNetCfg)
+		assert.Equal(t, tc.expected, err != nil, tc.name)
+	}
+}
+
+func TestValidator_Resource(t *testing.T) {
+	validator := NewValidator(nil, nil)
+
+	resource := validator.Resource()
+
+	assert.Contains(t, resource.OperationTypes, admissionregv1.Create)
+	assert.Contains(t, resource.OperationTypes, admissionregv1.Update)
 }
